db69b19c647432611ae3b3402c9b66ab10ca2a58
Hunter <hunvicter@gmail.com>
2024-07-17 13:52:40 +0700

    feat:resolve problems

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,12 @@
 function findStudentIndex(students, searchStudent) {
+	return students.indexOf(searchStudent); 
 	// เริ่มเขียนโค้ดตรงนี้จ้า
 }
 
+let result = findStudentIndex(students = ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"],"John")
+let result2 = findStudentIndex(students = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"],"Andrew")
+
+console.log(result);
+console.log(result2);
 // ตอบคำถามตรงนี้จ้า
+//O(1) จำนวนรอบเท่าเดิมไม่มีการเปลี่ยนแปลงกับข้อมูลที่ใส่เข้ามา
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,47 @@
-function findBookIndex(books, searchTitle) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+
+function findBookIndex(books, searchBook) {
+  // let search =0
+  let index = -1
+  for(let ans in books){
+    console.log(ans);
+    let answer = books[ans]
+    
+    console.log(answer);
+    console.log(answer.title);
+    if(answer.title === searchBook){
+      index = ans
+    }
+  }
+  return index
 }
+
+
+let result = findBookIndex([
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" }
+],"Gone with the Wind")
+console.log(result);
+
+
+let result2 = findBookIndex([
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" }
+],"The Master Mind")
+
+
+console.log(result2);
+
+
+//O(n) input เพิ่มขึ้นตามลำดับที่ใส่เข้ามา
